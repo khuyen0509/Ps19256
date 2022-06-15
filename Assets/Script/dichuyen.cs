6671edc27424a0ffe30b47129a78d02f5735b9ed
khuyen0509 <101702859+khuyen0509@users.noreply.github.com>
2022-06-15 16:49:16 +0700

    Initial commit

---
+++ Assets/Script/dichuyen.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class dichuyen : MonoBehaviour
+{
+
+    public Animator ani;
+    public bool isRight;
+    public bool nen_dat;
+
+    public GameObject gameOverButton;
+    public GameObject gameOverText;
+
+    public bool isPause = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ani.SetBool("isRunning", false);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isRight = true;
+            ani.SetBool("isRunning", true);
+
+            transform.Translate(Time.deltaTime * 5, 0, 0);
+            transform.localScale = new Vector3(1F, 1F, 1F);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            isRight = true;
+            ani.SetBool("isRunning", true);
+
+            transform.Translate(-Time.deltaTime * 5, 0, 0);
+            transform.localScale = new Vector3(-1F, 1F, 1F);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (nen_dat == true)
+            {
+                
+            
+
+                if (isRight == true)
+                {
+                    transform.Translate(Time.deltaTime * 5, 2.5f, 0);
+                }
+                else
+                {
+                    transform.Translate(-Time.deltaTime * 5, 2.5f, 0);
+                }
+                nen_dat = false;
+            }
+        }
+
+        if(Input.GetKey(KeyCode.P))
+        {
+            isPause = !isPause;
+            if (isPause)
+            {
+                Time.timeScale = 0;
+
+            }
+            else
+            {
+                Time.timeScale = 1;
+
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "nen_dat")
+        {
+            nen_dat = true;
+        }
+
+        if (collision.gameObject.tag == "qua_man")
+        {
+            SceneManager.LoadScene("Man1");
+        }
+
+        if (collision.gameObject.tag == "qua_man1")
+        {
+            SceneManager.LoadScene("Start");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "roi_xuong")
+        {
+            gameOverButton.SetActive(true);//khi chết button replay hiện ra
+            gameOverText.SetActive(true);//khi chết text game over  hiện ra
+            Time.timeScale = 0;// timescale = 0 ,trò chơi dừng =1 trò chơi chạy lại
+        }
+    }
+}
---
+++ Assets/Script/kill_monter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class kill_monter : MonoBehaviour
+{
+    public GameObject PsCoin;
+    public static int diemTong = 0;
+    public GameObject diemtext;
+
+    public GameObject gameOverButton;
+    public GameObject gameOverText;
+
+    public AudioSource sound_death;
+    public AudioSource sound_main;
+    public AudioSource sound_die;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CongDiem(0);
+        sound_main.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CongDiem(int diemCong)
+    {
+        diemTong += diemCong;
+        diemtext.GetComponent<Text>().text = "Điểm : " + diemTong.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string name_monter = collision.attachedRigidbody.gameObject.name;
+        if (collision.gameObject.tag == "ben_trai")
+        {
+            //sound_main.Stop();
+            //Play();
+            sound_main.Stop();
+            sound_death.Play();
+            Destroy(GameObject.Find("nhan_vat"));//nhan vat dung nam chet
+            gameOverButton.SetActive(true);//khi chết button replay hiện ra
+            gameOverText.SetActive(true);//khi chết text game over  hiện ra
+            Time.timeScale = 0;// timescale = 0 ,trò chơi dừng =1 trò chơi chạy lại
+        }
+        if (collision.gameObject.tag == "ben_tren")
+        {
+            CongDiem(1);
+            sound_die.Play();
+            Destroy(GameObject.Find(name_monter));
+            Instantiate(PsCoin, collision.gameObject.transform.position, collision.gameObject.transform.localRotation);
+        }
+
+        if (collision.gameObject.tag == "roi_xuong")
+        {
+            
+            Destroy(GameObject.Find("nhan_vat"));
+            sound_death.Play();
+            sound_main.Stop();
+            gameOverButton.SetActive(true);//khi chết button replay hiện ra
+            gameOverText.SetActive(true);//khi chết text game over  hiện ra
+            Time.timeScale = 0;// timescale = 0 ,trò chơi dừng =1 trò chơi chạy lại
+        }
+
+
+    }
+}
